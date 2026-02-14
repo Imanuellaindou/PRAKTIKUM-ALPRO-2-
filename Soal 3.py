ba81a1ac2c79376dd7af61a1a5e4e838e0342f2e
Imanuellaindou <71241110@students.ukdw.ac.id>
2026-02-14 21:54:08 +0700

    Delete Soal 3.py

--- Soal 3.py
+++
@@ -1,9 +0,0 @@
->>> import math
->>> saldo_awal = 200000000
->>> saldo_yang_diinginkan = 400000000
->>> bunga_per_tahun = 0.10
->>> tahun = math. log(saldo_yang_diinginkan / saldo_awal) / math.log(1 + bunga_per_tahun)
->>> t_rounded = math.ceil(tahun)
->>> print(f"waktu yang erika butuhkan adalah {t_rounded} tahun")
-waktu yang erika butuhkan adalah 8 tahun
->>> 
