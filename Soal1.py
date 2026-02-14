68c8f45855af7401881a6867ef5d900a320b80ba
Imanuellaindou <71241110@students.ukdw.ac.id>
2026-02-14 21:24:27 +0700

    Delete Soal1.py

--- Soal1.py
+++
@@ -1,4 +0,0 @@
-tinggi_badan = float(input("Masukkan tinggi badan anda (dalam meter): "))
-berat_badan = float(input("Masukkan berat badan anda (kg): "))
-hitung_bmi = berat_badan / (tinggi_badan ** 2)
-print(f"BMI Anda adalah: {hitung_bmi:.2f}")
